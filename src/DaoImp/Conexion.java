56f10b068a2cd1e726beec3e4bbd77a693b016ed
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2021-11-09 19:10:59 -0300

    conexion

---
+++ src/DaoImp/Conexion.java
@@ -0,0 +1,52 @@
+package DaoImp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+	public static Conexion instancia;
+	private Connection conexion;
+	
+	private Conexion()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver"); 
+			this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbtup","root","root");
+			this.conexion.setAutoCommit(false);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static Conexion getConexion()   
+	{								
+		if(instancia == null)
+		{
+			instancia = new Conexion();
+		}
+		return instancia;
+	}
+
+	public Connection getSQLConexion() 
+	{
+		return this.conexion;
+	}
+	
+	public void cerrarConexion()
+	{
+		try 
+		{
+			this.conexion.close();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		instancia = null;
+	}
+}
